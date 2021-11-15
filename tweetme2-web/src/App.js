9dbc5d3ba3361a25ef031ba4117165ed37b8d5eb
Hogun Ryu <i.love.it.6230@gmail.com>
2021-11-15 11:17:41 +0900

    재정비

---
+++ tweetme2-web/src/App.js
@@ -0,0 +1,62 @@
+import React, {useEffect, useState} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function loadTweets(callback) {
+  const xhr = new XMLHttpRequest() // xhr = SomeClass()
+  const method = 'GET' // "POST"
+  const url = 'http://localhost:8000/api/tweets/'
+  const responseType = 'json'
+
+  xhr.responseType = responseType
+  xhr.open(method, url) 
+  xhr.onload = function() {
+    callback(xhr.response, xhr.status)
+  }
+  xhr.onerror = function(e) {
+    console.log(e)
+    callback({'message':'해당 요청은 오류가 있습니다.'}, 400)
+  }
+  xhr.send()
+}
+
+function App() {
+  const [tweets, setTweets] = useState([])
+
+  useEffect(()=>{
+    const myCallback = (response, status) =>{
+      console.log(response, status)
+      if (status === 200) {
+        setTweets(response)
+      } else {
+        alert("오류가 있습니다.")
+      }
+    }
+    loadTweets(myCallback)
+  }, [])
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          {tweets.map((tweet, index)=>{
+            return <li>{tweet.content}</li>
+          })}
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
