fab76a78ad05026c6c90890221db35ff7af872f3
dannyryu4758 <i.love.it.6230@gmail.com>
2021-12-28 18:08:55 +0900

    test

--- profiles/views.py
+++ profiles/views.py
@@ -3,5 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 def profile_detail_view(request, username, *args, **kwargs) :
     # get the profile for the passed username
-    # temp
     return render(request, "profiles/detail.html", {"username" : username})
