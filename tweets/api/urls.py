57764b3faa2048dff8f3422b9035831b143ccd92
Hogun Ryu <i.love.it.6230@gmail.com>
2021-12-28 15:48:28 +0900

    08:36:00 82. Clean Up API Urls and Views

--- tweets/api/urls.py
+++ tweets/api/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from .views import (home_view, tweet_detail_view,
+from .views import ( tweet_detail_view,
                           tweet_list_view, tweet_create_view, tweet_delete_view, tweet_action_view,)
 
 urlpatterns = [
