3b789e151050c30a63883aed83d38fc3cc60d9bb
Hogun Ryu <i.love.it.6230@gmail.com>
2021-12-28 18:01:47 +0900

    temp

--- profiles/views.py
+++ profiles/views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def profile_detail_view(request, username, *args, **kwargs) :
     # get the profile for the passed username
+    # temp
     return render(request, "profiles/detail.html", {"username" : username})
