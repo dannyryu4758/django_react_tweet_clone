45746a68380c8c6bdbd735c224f499e731e88383
dannyryu4758 <i.love.it.6230@gmail.com>
2021-12-29 14:06:51 +0900

    10:09:29 92. Profile Following Unit Test Cases

--- profiles/api/views.py
+++ profiles/api/views.py
@@ -27,6 +27,9 @@ ALLOWED_HOSTS = settings.ALLOWED_HOSTS
 def user_follow_view(request, username, *args, **kwargs):
     me = request.user
     other_user_qs = User.objects.filter(username=username)
+    if me.username == username :
+        my_followers = me.profile.followers.all()
+        return Response({"count": my_followers.count()}, status=200)
     if not other_user_qs.exists() :
         return Response({}, status=404)
     other = other_user_qs.first()
@@ -40,4 +43,4 @@ def user_follow_view(request, username, *args, **kwargs):
     else :
         pass
     current_followers_qs = profile.followers.all()
-    return Response({"count": current_followers_qs.count()}, status=400)
+    return Response({"count": current_followers_qs.count()}, status=200)
--- profiles/tests.py
+++ profiles/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from rest_framework.test import APIClient
 
 # Create your tests here.
 from django.contrib.auth import get_user_model
@@ -10,7 +11,12 @@ User = get_user_model()
 class ProfileTestCase(TestCase):
     def setUp(self) :
         self.user = User.objects.create_user(username="abc", password="somepassword")
-        self.user_b = User.objects.create_user(username="abc2", password="somepassword2")
+        self.userb = User.objects.create_user(username="abc2", password="somepassword2")
+
+    def get_client(self) :
+        client = APIClient()
+        client.login(username=self.user.username, password="somepassword")
+        return client
 
     def test_profile_created_via_signal(self) :
         qs = Profile.objects.all()
@@ -18,7 +24,7 @@ class ProfileTestCase(TestCase):
 
     def test_following(self):
         first = self.user
-        second = self.user_b
+        second = self.userb
         first.profile.followers.add(second) # added a follower
         second_user_following_whom = second.following.all()
         qs = second_user_following_whom.filter(user=first) # from a user, check other user is being followed.
@@ -26,3 +32,36 @@ class ProfileTestCase(TestCase):
         self.assertTrue(qs.exists())
         self.assertFalse(first_user_following_no_one.exists())
 
+    def test_follow_api_endpoint(self):
+        client = self.get_client()
+        response = client.post(
+            f"/api/profiles/{self.userb.username}/follow/",
+            {"action": "follow"}
+        )
+        r_data = response.json()
+        count = r_data.get("count")
+        self.assertEqual(count, 1)
+
+    def test_unfollow_api_endpoint(self):
+        first = self.user
+        second = self.userb
+        first.profile.followers.add(second) # added a follower
+
+        client = self.get_client()
+        response = client.post(
+            f"/api/profiles/{self.userb.username}/follow/",
+            {"action": "unfollow"}
+        )
+        r_data = response.json()
+        count = r_data.get("count")
+        self.assertEqual(count, 0)
+
+    def test_cannot_follow_api_endpoint(self):
+        client = self.get_client()
+        response = client.post(
+            f"/api/profiles/{self.user.username}/follow/",
+            {"action": "follow"}
+        )
+        r_data = response.json()
+        count = r_data.get("count")
+        self.assertEqual(count, 0)
