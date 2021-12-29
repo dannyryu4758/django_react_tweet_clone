691c8d483053c497e15aced8a0a3b4af755cafbc
dannyryu4758 <i.love.it.6230@gmail.com>
2021-12-29 10:16:32 +0900

    09:21:43 88. Save 2 Models in 1 Form and 1 View

--- profiles/views.py
+++ profiles/views.py
@@ -1,7 +1,37 @@
 from django.http import Http404
-from django.shortcuts import render
-
+from django.shortcuts import redirect, render
+from .forms import ProfileForm
 from profiles.models import Profile
+
+
+def profile_update_view(request, *args, **kwargs) :
+    if not request.user.is_authenticated : # is_authenticated()
+        return redirect("/login?next=/profile/update")
+    user = request.user
+    user_date = {
+        "first_name" : user.first_name,
+        "last_name" : user.last_name,
+        "email" : user.email,
+    }
+    my_profile = request.user.profile
+    form = ProfileForm(request.POST or None, instance=my_profile, initial=user_date)
+    if form.is_valid() :
+        profile_obj = form.save(commit=False)
+        first_name = form.cleaned_data.get('first_name')
+        last_name = form.cleaned_data.get('last_name')
+        email = form.cleaned_data.get('email')
+        user.first_name = first_name
+        user.last_name = last_name
+        user.email = email
+        user.save()
+        profile_obj.save()
+    context = {
+        "form" : form,
+        "btn_label" : "Save",
+        "title" : "Update Pofile",
+    }
+    return render(request, "profiles/form.html", context)
+
 
 # Create your views here.
 def profile_detail_view(request, username, *args, **kwargs) :
