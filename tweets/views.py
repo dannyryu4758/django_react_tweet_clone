386d4034b9d1b52d1af69850644d1786d89ab930
dannyryu4758 <i.love.it.6230@gmail.com>
2021-12-30 14:51:07 +0900

    11:29:20 102. Build for the Feed

--- tweets/views.py
+++ tweets/views.py
@@ -7,11 +7,7 @@ ALLOWED_HOSTS = settings.ALLOWED_HOSTS
 
 # Create your views here.
 def home_view(request, *args, **kwargs):
-    # return HttpResponse("<h1>Home Page<h1>")
-    username = None
-    if request.user.is_authenticated:
-        username = request.user.username
-    return render(request, "pages/home.html", context={"username": username}, status=200)
+    return render(request, "pages/feed.html")
 
 
 def tweets_list_view(request, *args, **kwargs):
